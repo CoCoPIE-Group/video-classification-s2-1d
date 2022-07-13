40210e809b6363c0a398d0876c59d751af467d18
tanqy <qiaoyu.tan@cocopie.ai>
2022-07-13 18:13:00 +0800

    [fix]xgen_prepare_dataset.

--- xgen_prepare_dataset.sh
+++ xgen_prepare_dataset.sh
@@ -10,3 +10,6 @@ wget https://xgen.oss-cn-hongkong.aliyuncs.com/data/video-classification-s2-1d/U
 mkdir ucf101 && unrar e UCF101.rar ucf101
 unzip UCF101TrainTestSplits-RecognitionTask.zip
 
+python /root/Projects/video-classification-s2-1d/extract_videos.py -d ucf101
+python /root/Projects/video-classification-s2-1d/create_lmdb.py -d ucf101_frame -s train -vr 0 10000
+python /root/Projects/video-classification-s2-1d/create_lmdb.py -d ucf101_frame -s val -vr 0 4000
