1385f5099fc4ff0b30260e5edc41d4c365a42377
liumaolin <maolin.liu@cocopie.ai>
2022-10-26 12:11:26 +0800

    [feat]Support singularity.

--- xgen_prepare_dataset.sh
+++ xgen_prepare_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-data_path="/data/video-classification-s2-1d"
+data_path="/xgen/data/video-classification-s2-1d"
 mkdir ${data_path} && cd ${data_path}
 
 wget https://xgen.oss-cn-hongkong.aliyuncs.com/data/video-classification-s2-1d/UCF101.rar
@@ -10,6 +10,6 @@ wget https://xgen.oss-cn-hongkong.aliyuncs.com/data/video-classification-s2-1d/U
 mkdir ucf101 && unrar e UCF101.rar ucf101
 unzip UCF101TrainTestSplits-RecognitionTask.zip
 
-python /root/Projects/video-classification-s2-1d/extract_videos.py -d ucf101
-python /root/Projects/video-classification-s2-1d/create_lmdb.py -d ucf101_frame -s train -vr 0 10000
-python /root/Projects/video-classification-s2-1d/create_lmdb.py -d ucf101_frame -s val -vr 0 4000
+python /xgen/Projects/video-classification-s2-1d/extract_videos.py -d ucf101
+python /xgen/Projects/video-classification-s2-1d/create_lmdb.py -d ucf101_frame -s train -vr 0 10000
+python /xgen/Projects/video-classification-s2-1d/create_lmdb.py -d ucf101_frame -s val -vr 0 4000
